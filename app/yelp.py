a3f0c7a7c1259ddb5e4f4be98f1c378a58dc3140
Humphrey Obuobi <obuobhum@gmail.com>
2017-01-18 01:39:07 -0500

    using environment variables for yelp

--- app/yelp.py
+++ app/yelp.py
@@ -2,8 +2,17 @@
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 import io, os, json
-
+'''
 with io.open('app/config.json') as cred:
     creds = json.load(cred)
     auth = Oauth1Authenticator(**creds)
     client = Client(auth)
+'''
+auth = Oauth1Authenticator(
+    consumer_key=os.environ['CONSUMER_KEY'],
+    consumer_secret=os.environ['CONSUMER_SECRET'],
+    token=os.environ['TOKEN'],
+    token_secret=os.environ['TOKEN_SECRET']
+)
+
+client = Client(auth)
